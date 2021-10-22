dc47805fb4cbea7bae8e5f6370803a3a199ef9d7
unknown <humbertoley28@gmail.com>
2021-10-22 15:25:04 -0600

    cambios

---
+++ Signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Style.css" rel="stylesheet" type="text/css" />
+    <title>Overcrowded</title>
+</head>
+<div id="login-box">
+<div id="cuadro">
+</div>
+  <div class="left">
+    <h1>Sign up</h1>
+    <input type="text" name="username" placeholder="Username" />
+    <input type="text" name="name" placeholder="Name" />
+    <input type="text" name="last name" placeholder="Last name" />  
+    <input type="text" name="email" placeholder="E-mail" />
+    <input type="text" name="phone" placeholder="Phone" />  
+    <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="password2" placeholder="Retype password" />
+    
+    <input type="submit" name="signup_submit" value="Sign me up" />
+  </div>
+ 
+</div>
+</html>
+
