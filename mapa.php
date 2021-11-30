6fa4d2b2561c4b4d36a59336c5d62fdb7ec648ba
sergio-leyva <80796075+sergio-leyva@users.noreply.github.com>
2021-11-30 13:01:01 -0600

    sergio

--- mapa.php
+++ mapa.php
@@ -1,17 +1,19 @@
-
-
-
+<?php
+  include("conexionBD/conexion.php");
+  $reportes="SELECT * FROM marcadores";
+?>
 <!DOCTYPE html>
 <html>
 <head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0">
 	<link rel="stylesheet" href="css/mapa.css">
-    <title>Maps JavaScript API</title>
-
- 
+    <title>Maps JavaScript API</title> 
 </head>  
 	<body>
+  <?php 
+  $resultado = mysqli_query($connLocalhost, $reportes);
+  while($row=mysqli_fetch_assoc($resultado)){?>
 	<script async defer
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqVwIR2LNGqiYsdV-rVNB5Kh3lM0dWYAM&callback=initMap">
     </script>
@@ -25,16 +27,13 @@
           zoom: 13
         });
 
-		var marker = new google.maps.Marker({
-          position:  {lat: 28.006381, lng: -110.921847},
+		  var marker = new google.maps.Marker({
+          position: new google.maps.LatitudLongitud(<?php echo $row['latitud']; ?>, <?php echo $row['longitud']; ?>),
           map: map,
-	  title: 'Mi ubicacion'
+	      title: <?php echo $row['direccion'];?>
         });
-     
-        
-      
       }
-
 	</script>
+  <?php }?>
 	</body> 
 </html>
