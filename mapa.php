bedf291af9d2e5b658b8001a43ced7f41a9c728d
sergio-leyva <80796075+sergio-leyva@users.noreply.github.com>
2021-12-01 18:36:43 -0600

    sergio

--- mapa.php
+++ mapa.php
@@ -1,3 +1,27 @@
+<?php
+	/* Database connection settings */
+	$host = 'bowgqfdjymwtjrai1vil-mysql.services.clever-cloud.com';
+	$user = 'uqqpgtuobtnpuiii';
+	$pass = 'zqbdbi78oaeWDbHAHcHs';
+	$db = 'bowgqfdjymwtjrai1vil';
+	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+ 	$coordinates = array();
+ 	$latitudes = array();
+ 	$longitudes = array();
+
+	// Select all the rows in the markers table
+	$query = "SELECT  `latitud`, `longitud` FROM `marcadores` ";
+	$result = $mysqli->query($query) or die('data selection for google map failed: ' . $mysqli->error);
+
+ 	while ($row = mysqli_fetch_array($result)) {
+
+		$latitudes[] = $row['latitud'];
+		$longitudes[] = $row['longitud'];
+		$coordinates[] = 'new google.maps.LatLng(' . $row['latitud'] .','. $row['longitud'] .'),';
+	}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -17,12 +41,16 @@
       function initMap() {
 		
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 28.006381, lng: -110.921847},
-          zoom: 13
+          zoom: 18,
+		center: {<?php echo'lat:'. $latitudes[0] .', lng:'. $longitudes[0] ;?>}, //{lat: --- , lng: ....}
+		 mapTypeId: google.maps.MapTypeId.SATELITE
         });
          
+		
+		startPoint = {<?php echo'latitud:'. $latitudes[0] .', longitud:'. $longitudes[0] ;?>};
+		
 		var marker = new google.maps.Marker({
-          position:  {lat: 28.006381, lng: -110.921847},
+          position:  startPoint,
           map: map,
 	  title: 'Mi ubicacion'
         });
