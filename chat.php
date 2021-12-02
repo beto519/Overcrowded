9741f4abe33939f4ee4a763838cdeac319c399c9
beto519 <jesusalbertoriveral205530@gmail.com>
2021-12-01 19:08:52 -0600

    beto519
    ya esta chat

---
+++ chat.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+</head>
+<body>
+
+ <?php
+ 
+ include("conexionBD/conexion.php");
+
+
+
+ 
+    $consulta = "SELECT * FROM mensajes ;";
+    $resQueryMessage = mysqli_query($connLocalhost, $consulta) or trigger_error("El query falló");
+        while ($fila = $resQueryMessage->fetch_array()): 
+?>
+    <div id="data-chat">
+            <span style="color:#1c62c4;"><?php echo $fila['usuario'] ?>: </span>
+             <span style="color:#848484;"><?php echo $fila['texto'] ?></span>
+            
+    </div>
+<?php endwhile;?>
+</body>
+</html>
--- formulario.php
+++ formulario.php
@@ -85,14 +85,61 @@
   </p>
   <p class='field half'>
     <label class='label' for='select'></label>
- 
-    <p  id="latitud" name="latitud"><?php if(isset($_POST['latitud'])) echo $_POST['latitud']; ?></p>
-  <p  id="longitud" name="name"></p> <?php if(isset($_POST['longitud'])) echo $_POST['longitud']; ?></p>
+
+
+
+   <p  id="latitud" name="latitud"></p>
+  <p  id="longitud" name="name"></p></p>
   <p class='field half'>
-    <input class='button' id="send"  name="send" type='submit' value='Enviar'>
+
+<input class='button' id="send"  name="send" type='submit' value='Enviar'>
   </p>
+
 </form>
 
-<script src="script.js"></script>
+<script>
+  
+
+const funcionInit = () => {
+	if (!"geolocation" in navigator) {
+		return alert("Tu navegador no soporta el acceso a la ubicación. Intenta con otro");
+	}
+
+	const $latitud = document.querySelector("#latitud"),
+		$longitud = document.querySelector("#longitud"),
+		$enlace = document.querySelector("#enlace");
+
+
+	const onUbicacionConcedida = ubicacion => {
+		console.log("Tengo la ubicación: ", ubicacion);
+		const coordenadas = ubicacion.coords;
+		$latitud.innerText = coordenadas.latitude;
+		$longitud.innerText = coordenadas.longitude;
+		
+	}
+	const onErrorDeUbicacion = err => {
+
+		$latitud.innerText = "Error obteniendo ubicación: " + err.message;
+		$longitud.innerText = "Error obteniendo ubicación: " + err.message;
+		console.log("Error obteniendo ubicación: ", err);
+	}
+
+	const opcionesDeSolicitud = {
+		enableHighAccuracy: true, // Alta precisión
+		maximumAge: 0, // No queremos caché
+		timeout: 5000 // Esperar solo 5 segundos
+	};
+
+	$latitud.innerText = "Cargando...";
+	$longitud.innerText = "Cargando...";
+	navigator.geolocation.getCurrentPosition(onUbicacionConcedida, onErrorDeUbicacion, opcionesDeSolicitud);
+
+};
+document.addEventListener("DOMContentLoaded", funcionInit);
+
+</script>
+
+
+
 </body>
 </html>
